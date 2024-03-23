27dc1cc6d8b399ee25fca47b481f1e362204c60f
priyanshi <namdevpriyanshi059@gmail.com>
2024-03-23 18:16:50 +0530

    add signout functionality

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -5,7 +5,7 @@ import {getDownloadURL, getStorage, uploadBytesResumable,ref} from 'firebase/sto
 import { app } from '../firebase';
 import { CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
-import { updateStart, updateSuccess, updateFailure, deleteUserStart, deleteUserFailure, deleteUserSuccess } from '../redux/user/userSlice';
+import { updateStart, updateSuccess, updateFailure, deleteUserStart, deleteUserFailure, deleteUserSuccess, signoutSuccess } from '../redux/user/userSlice';
 import {HiOutlineExclamationCircle} from 'react-icons/hi'
 
 export default function DashProfile() {
@@ -143,6 +143,23 @@ export default function DashProfile() {
     }
   }
 
+  const handleSignout = async()=>{
+    try {
+      const res= await fetch(`/api/user/signout`,{
+        method:'POST',
+      });
+      const data = res.json();
+      if(!res.ok){
+        console.log(data.message);
+      }
+      else{
+        dispatch(signoutSuccess());
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+
 
   return (
     <div className='max-w-lg mx-auto p-3 w-full'>
@@ -186,7 +203,7 @@ export default function DashProfile() {
       </form>
       <div className='text-red-500 flex justify-between mt-5'>
         <span onClick={()=>setShowModal(true)} className='cursor-pointer'>Delete Account</span>
-        <span className='cursor-pointer'>Sign Out</span>
+        <span onClick = {handleSignout} className='cursor-pointer'>Sign Out</span>
       </div>
       {
         updateUserSuccess &&
--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -2,10 +2,13 @@ import {Sidebar} from 'flowbite-react';
 import {HiUser, HiArrowSmRight} from 'react-icons/hi'
 import { useState,useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
+import { signoutSuccess } from '../redux/user/userSlice';
+import { useDispatch } from 'react-redux';
 
 export default function DashSidebar() {
   const location = useLocation();
   const [tab, setTab] = useState('');
+  const dispatch = useDispatch();
   // we want to use useeffect reat hook so each time we come to this page we want to get its tab
   useEffect(()=>{
     // we use useSearchParams constructor to get search
@@ -17,6 +20,22 @@ export default function DashSidebar() {
       setTab(tabFromUrl)
     }
   },[location.search])
+  const handleSignout = async()=>{
+    try {
+      const res= await fetch(`/api/user/signout`,{
+        method:'POST',
+      });
+      const data = res.json();
+      if(!res.ok){
+        console.log(data.message);
+      }
+      else{
+        dispatch(signoutSuccess());
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
   return (
     <Sidebar className='w-full md:w-56'>
       <Sidebar.Items>
@@ -24,7 +43,7 @@ export default function DashSidebar() {
           <Link to='/dashboard?tab=profile'>
             <Sidebar.Item active={tab === 'profile'} icon={HiUser} label={'User'} labelColor = 'dark' as='div'>Profile</Sidebar.Item>
           </Link>
-          <Sidebar.Item  icon={HiArrowSmRight} className='cursor-pointer'>Sign Out</Sidebar.Item>
+          <Sidebar.Item  icon={HiArrowSmRight} className='cursor-pointer' onClick={handleSignout}>Sign Out</Sidebar.Item>
         </Sidebar.ItemGroup>
       </Sidebar.Items>
     </Sidebar>
