b48b499c8b88ecf937886478249b8830ae4462ca
priyanshi <namdevpriyanshi059@gmail.com>
2024-03-24 17:39:25 +0530

    add pos to the dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom"
 import DashSidebar from "../components/DashSidebar";
 import DashProfile from '../components/DashProfile';
+import DashPosts from "../components/DashPosts";
 
 export default function Dashboard() {
   const location = useLocation();
@@ -24,8 +25,10 @@ export default function Dashboard() {
         {/* Sidebar */}
         <DashSidebar/>
       </div>
-      {/* progile  ...*/}
+      {/* profle  ...*/}
       {tab === 'profile' && <DashProfile/>}
+      {/* posts */}
+      {tab === 'posts' && <DashPosts/>}
     </div>
   )
 }
