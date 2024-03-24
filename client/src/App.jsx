fb6cfda19a3b1d6b19a828b046c8409dc108d934
priyanshi <namdevpriyanshi059@gmail.com>
2024-03-24 12:38:16 +0530

    compelete create a post page ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,8 @@ import SignIn from './pages/Signin'
 import SignUp from './pages/SignUp'
 import Footer from './components/Footer'
 import PrivateRoute from './components/PrivateRoute'
+import OnlyAdminPrivateRoute from './components/OnlyAdminPrivateRoute'
+import CreatePost from './pages/CreatePost'
 
 export default function App() {
   return (
@@ -23,6 +25,9 @@ export default function App() {
         <Route element={<PrivateRoute/>}>
           <Route path="/dashboard" element={<Dashboard/>}/>
         </Route>
+        <Route element={<OnlyAdminPrivateRoute/>}>
+          <Route path="/create-post" element={<CreatePost/>}/>
+        </Route>        
         <Route path="/projects" element={<Projects/>}/>
       </Routes>
     
